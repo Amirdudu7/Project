1aa26e21ff5c89a02ba9dab9d5b7ee0cde49f261
PlutoniumBatman <alieagh01234@gmail.com>
2025-11-20 22:40:42 +0330

    sec5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityDashboard from "../../features/activities/dashboard/ActivityDashb
 function App() {
   const [activities,setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect (() => {
     axios.get<Activity[]>('https://localhost:5001/api/activities')
@@ -20,15 +21,44 @@ function App() {
   const handleCancelSelectActivity = () => {
     setSelectedActivity(undefined);
   }
+  const handleOpenForm = (id?: string) => {
+    if (id) handleSelectActivity(id);
+    else handleCancelSelectActivity();
+    setEditMode(true);
+  }
+  const handleFormClose = () => {
+    setEditMode(false);
+  }
+  const handleSubmitForm = (activity: Activity) => {
+    if (activity.id){
+      setActivities(activities.map(x => x.id === activity.id? activity : x))
+    }else{
+      const newActivity = {...activity, id:activities.length.toString()}
+      setSelectedActivity(newActivity);
+      setActivities([...activities, newActivity])
+    }
+    setEditMode(false);
+  }
+  const handleDelete = (id: string) => {
+    setActivities(activities.filter(x => x.id !== id))
+  }
 
   return (
     <Box sx={{bgcolor:'#eeeeee'}}>
       <CssBaseline/>
-      <NavBar />
+      <NavBar openForm={handleOpenForm} />
       <Container maxWidth='xl' sx={{mt:3}}>
-        <ActivityDashboard activities={activities} selectActivity={handleSelectActivity} 
+        <ActivityDashboard 
+          activities={activities} 
+          selectActivity={handleSelectActivity} 
           cancelSelectActivity={handleCancelSelectActivity}
-          selectedActivity={selectedActivity}/>
+          selectedActivity={selectedActivity}
+          editMode={editMode}
+          openForm={handleOpenForm}
+          closeForm={handleFormClose}
+          submitForm={handleSubmitForm}
+          deleteActivity={handleDelete}
+        />
       </Container>
     </Box>
   )
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid2} from '@mui/material'
 import ActivityList from './ActivityList';
 import ActivityDetail from '../details/ActivityDetail';
+import ActivityForm from '../form/ActivityForm';
 
 
 type Props = {
@@ -8,17 +9,44 @@ type Props = {
     selectActivity: (id: string) => void;
     cancelSelectActivity: () => void;
     selectedActivity?: Activity;
+    openForm: (id: string) => void;
+    closeForm: () => void;
+    editMode: boolean
+    submitForm: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
-export default function ActivityDashboard({activities, cancelSelectActivity, selectActivity, 
-    selectedActivity}: Props) {
+export default function ActivityDashboard({activities, cancelSelectActivity, 
+                        selectActivity, 
+                        selectedActivity,
+                        openForm,
+                        closeForm,
+                        editMode,
+                        submitForm,
+                        deleteActivity
+                      }: Props) {
   return (
     <Grid2 container justifyContent='space-between'>
         <Grid2 size={7}>
-            <ActivityList activities={activities} selectActivity={selectActivity}/>
+            <ActivityList 
+              activities={activities} 
+              selectActivity={selectActivity}
+              deleteActivity={deleteActivity}
+            />
         </Grid2>
         <Grid2 size={4}>
-          {selectedActivity && <ActivityDetail activity={selectedActivity} 
-          cancelSelectActivity={cancelSelectActivity} />}
+          {selectedActivity && !editMode && //detail only appears when something is selected and you're not editing.
+            <ActivityDetail 
+              activity={selectedActivity} 
+              cancelSelectActivity={cancelSelectActivity} 
+              openForm={openForm}
+            />
+          }
+          {editMode && //show Form And it passes down the selected activity so you can edit it.
+          <ActivityForm 
+            closeForm={closeForm} 
+            activity={selectedActivity}
+            submitForm={submitForm}
+          />}
         </Grid2>
     </Grid2>
   )
