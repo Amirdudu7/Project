bd3ef9125468f27b69043e6b0f83bc17738ab085
PlutoniumBatman <alieagh01234@gmail.com>
2025-11-24 10:54:50 +0330

    sec10.5

---
+++ client/src/features/activities/form/categoryOptions.ts
@@ -0,0 +1,9 @@
+
+export const categoryOptions = [
+    {text: 'Drinks', vlaue: 'drinks'},
+    {text: 'Culture', vlaue: 'culture'},
+    {text: 'Film', vlaue: 'film'},
+    {text: 'Food', vlaue: 'food'},
+    {text: 'Music', vlaue: 'music'},
+    {text: 'Travel', vlaue: 'travel'}
+]
